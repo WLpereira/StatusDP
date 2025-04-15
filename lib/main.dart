69c3ab560df554222aa6e3452ef96878f5c12de5
Rodrigo Moretto <rodrigo-moretto@hotmail.com>
2025-04-15 00:23:50 -0300

    Alteração de título da Página

--- lib/main.dart
+++ lib/main.dart
@@ -17,7 +17,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'StatusDP',
+      title: 'Planner DP',
       theme: ThemeData.dark(),
       home: const LoginScreen(),
     );
